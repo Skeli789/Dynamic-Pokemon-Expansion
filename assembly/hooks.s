6435f73750f730682661ae7506b3e52e4f796045
Skeli <sesyl.schweitzer@gmail.com>
2019-08-25 03:16:55 -0400

    Add Missing Egg Moves

--- assembly/hooks.s
+++ assembly/hooks.s
@@ -65,17 +65,7 @@ DexFlagCheckCall:
 	ldr r3, =0x8104AB0 | 1
 	bx r3
 
-@0x8045C58 with r0
-UpdateEggMoveLimit:
-	add r0, r2, #0x1
-	lsl r0, r0, #0x10
-	lsr r2, r0, #0x10
-	push {r2}
-	bl GetUpdatedEggMoveLimit
-	pop {r2}
-	ldr r1, =0x8045C60 | 1
-	bx r1
-
+.pool
 @0x800ECF2 with r4
 LoadSpecialPokePicHook:
 	mov r7, r8
@@ -91,6 +81,32 @@ LoadSpecialPokePicHook:
 	pop {r0}
 	bx r0
 
+.pool
+@0x8034356 with r1
+BattleLoadOpponentMonSpriteGfxHook:
+	ldr r1, [sp] @Personality
+	bl TryGetFemaleGenderedSpecies
+	mov r5, r0
+	ldr r6, [sp]
+	ldr r0, =0x803436C | 1
+	bx r0
+
+/*
+.pool
+@0x80747B4 with r0
+GetBattlerSpriteFinalYHook:
+	lsl r2, r2, #0x18
+	lsr r7, r2, #0x18
+	mov r0, r6
+	lsr r1, r1, #0x10
+	bl TryGetFemaleGenderedSpeciesFromBank
+
+	mov r0, r6
+	ldr r1, =0x80747BC | 1
+	bx r1
+*/
+
+.pool
 @0x8096E38 with r2
 GenderedMonIconHook:
 	lsr r7, r3, #0x10
@@ -106,4 +122,33 @@ GenderedMonIconHook:
 	ldr r1, [sp, #0x50] @Personality
 	ldr r0, =0x8096E40 | 1
 	bx r0
-	
+
+.pool
+@0x8139DDC with r1
+SummaryScreenIconPalHook:
+	mov r4, r0
+	mov r1, r0
+	mov r0, r5
+	bl TryGetFemaleGenderedSpecies
+	mov r5, r0
+	ldr r1, =SafeLoadMonIconPalette
+	bl bxr1
+	ldr r0, =0x8139DE4 | 1
+	bx r0
+
+.pool
+@0x808398C with r1
+CreateMonSprite_PicBoxHook:
+	mov r4, r0
+	mov r1, #0x0
+	mov r2, #0x80
+	lsl r2, #0x8 @0x8000
+	bl GetMonSpritePalStructFromOtIdPersonality
+	mov r1, r0
+	mov r0, r4
+	mov r4, r1 @Backup palette struct
+	ldrh r1, [r4, #0x4] @Tag
+	mov r3, #0x80
+	lsl r3, #0x8 @0x8000
+	ldr r2, =0x8083994 | 1
+	bx r2
