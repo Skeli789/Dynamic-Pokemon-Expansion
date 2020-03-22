7009e0cb3f0b062d30419498706fc96a1b69c5e7
Skeli <sesyl.schweitzer@gmail.com>
2020-03-22 22:54:02 +0200

    New Insertion Offset

--- scripts/make.py
+++ scripts/make.py
@@ -9,7 +9,7 @@ import sys
 ############
 
 ROM_NAME = "Pokemon Unbound.gba"  # The name of your rom
-OFFSET_TO_PUT = 0x1711fd0
+OFFSET_TO_PUT = 0x1712260
 SEARCH_FREE_SPACE = False  # Set to True if you want the script to search for free space
                            # Set to False if you don't want to search for free space as you update the engine
 
