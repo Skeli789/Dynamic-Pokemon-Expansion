838811d28689bf20bb0ba409b8ca5201f0a7dfdb
professorleondias <57677889+professorleondias@users.noreply.github.com>
2020-05-28 17:33:28 -0400

    Update Front_Pic_Coords_Table.c
    Commented out Gen 8

--- src/Front_Pic_Coords_Table.c
+++ src/Front_Pic_Coords_Table.c
@@ -5513,7 +5513,7 @@ const struct MonCoords gMonFrontPicCoords[NUM_SPECIES] =
 		.size = 0x0,
 		.y_offset = 0x0,
 	},
-	[SPECIES_GROOKEY] =
+/*	[SPECIES_GROOKEY] =
 	{
 		.size = 0x0,
 		.y_offset = 0x0,
@@ -6298,4 +6298,5 @@ const struct MonCoords gMonFrontPicCoords[NUM_SPECIES] =
 		.size = 0x0,
 		.y_offset = 0x0,
 	},
+*/
 };
