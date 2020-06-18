9a2e37c1314983282614e09e539fb63d1fd10561
professorleondias <57677889+professorleondias@users.noreply.github.com>
2020-06-17 21:42:34 -0400

    Update Back_Pic_Coords_Table.c

--- src/Back_Pic_Coords_Table.c
+++ src/Back_Pic_Coords_Table.c
@@ -5918,22 +5918,22 @@ const struct MonCoords gMonBackPicCoords[NUM_SPECIES] =
 		.size = 0x0,
 		.y_offset = 0x0,
 	},
-	[SPECIES_ZARUDE] =
-	{
-		.size = 0x0,
-		.y_offset = 0x0,
-	},
-	[SPECIES_UNKNOWN_MYTHICAL] =
-	{
-		.size = 0x0,
-		.y_offset = 0x0,
-	},
 	[SPECIES_KUBFU] =
 	{
 		.size = 0x0,
 		.y_offset = 0x0,
 	},
 	[SPECIES_URSHIFU_SINGLE] =
+	{
+		.size = 0x0,
+		.y_offset = 0x0,
+	},
+		[SPECIES_ZARUDE] =
+	{
+		.size = 0x0,
+		.y_offset = 0x0,
+	},
+	[SPECIES_UNKNOWN_MYTHICAL] =
 	{
 		.size = 0x0,
 		.y_offset = 0x0,
