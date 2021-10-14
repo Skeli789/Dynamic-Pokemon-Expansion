23f4ffa633c833910088d23964c174d73a77a1f7
Skeli <sesyl.schweitzer@gmail.com>
2021-10-14 17:21:48 -0400

    Fix Incorrect Constants

--- scripts/tm_tutor.py
+++ scripts/tm_tutor.py
@@ -5,7 +5,7 @@ from glob import glob
 # Data
 TM_HM_COUNT = 128
 TUTOR_COUNT = 128
-SPECIES_COUNT = 0x4EC + 1
+SPECIES_COUNT = 0x4F3 + 1
 
 TM_OUTPUT = "assembly/generated/tm_compatibility.s"
 TUTOR_OUTPUT = "assembly/generated/tutor_compatibility.s"
