dc9ce5be12a803c61a6041737de064a3619ad9eb
Skeli <sesyl.schweitzer@gmail.com>
2022-01-02 13:51:02 +0200

    Other Unbound Fixes & Updates
    -Hook in SeenAllRegionalMons funcs for scripts repo
    -Prevent Gen 8 mons from affecting dex count
    -Nickit & Thievul Technician
    _Learnset updates
    -Fix incorrect Borrius dex positions

--- special_inserts.asm
+++ special_inserts.asm
@@ -4,6 +4,7 @@
 
 .equ EVOS_PER_MON, 16
 .equ FINAL_DEX_ENTRY, 898
+.equ FINAL_GEN_7_DEX_ENTRY, 809
 
 @;.equ SEEN_DEX_FLAGS, 0x202583C
 @;.equ CAUGHT_DEX_FLAGS, (SEEN_DEX_FLAGS + (FINAL_DEX_ENTRY / 8) + 1)
@@ -39,7 +40,7 @@ add r3, #EVOS_PER_MON * 8
 
 @@Fix Dex Views@@
 .org 0x88EA4, 0xFF @Pokedex Count
-.word FINAL_DEX_ENTRY - 1
+.word FINAL_GEN_7_DEX_ENTRY - 1
 
 .org 0x1037D4, 0xFF @Weight View
 .word FINAL_DEX_ENTRY - 1
